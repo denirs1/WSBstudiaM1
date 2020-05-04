9468e3ac28b8165ff73beb0213fd04cd4daffc17
denirs1 <denis2215@gmail.com>
2020-05-05 00:38:52 +0200

    zadanie
    zadanie sekcja 1

---
+++ zadaniesekcja1/src/main/java/com/example/zadaniejeden/ZadaniejedenApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadaniejeden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadaniejedenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadaniejedenApplication.class, args);
+	}
+
+}
---
+++ zadaniesekcja1/src/test/java/com/example/zadaniejeden/ZadaniejedenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadaniejeden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadaniejedenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
